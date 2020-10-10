b82c5ceaea19970c2508adca8879e1c5cd223d9b
Helen Xu <helen.z.xu@duke.edu>
2020-10-09 22:14:59 -0400

    worked on HashListAutocomplete class

---
+++ src/HashListAutocomplete.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class HashListAutocomplete implements Autocompletor{
+    private static final int MAX_PREFIX = 10;
+    private Map<String,List<Term>> myMap;
+    private int mySize;
+
+    public HashListAutocomplete(String[] terms, double[] weights) {
+        if (terms == null || weights == null) {
+            throw new NullPointerException("One or more arguments null");
+        }
+
+        initialize(terms,weights);
+    }
+
+    @Override
+    public List<Term> topMatches(String prefix, int k) {
+        if (prefix.length() > MAX_PREFIX)
+            prefix = prefix.substring(0,MAX_PREFIX+1);
+        if (!myMap.containsKey(prefix))
+            return new ArrayList<>();
+        List<Term> all = myMap.get(prefix);
+        return all.subList(0,Math.min(k,all.size()));
+    }
+
+    @Override
+    public void initialize(String[] terms, double[] weights) {
+        for (int j=0;j<terms.length;j++) {
+            for (int i = 1; i < MAX_PREFIX + 1; i++) {
+                String prefix = terms[j].substring(0,i);
+                myMap.putIfAbsent(prefix,new ArrayList<>());
+                myMap.get(prefix).add(new Term(terms[j], weights[j]));
+            }
+        }
+        for (String key : myMap.keySet()) {
+            List<Term> list = myMap.get(key);
+            Collections.sort(list,Comparator.comparing(Term::getWeight).reversed());
+        }
+    }
+
+    @Override
+    public int sizeInBytes() {
+        if (mySize == 0) {
+            for(String key : myMap.keySet()) {
+                mySize += key.length()*BYTES_PER_CHAR + BYTES_PER_DOUBLE;
+                for (Term t:myMap.get(key))
+                    mySize += t.getWord().length()*BYTES_PER_CHAR;
+            }
+        }
+        return mySize;
+    }
+
+}
